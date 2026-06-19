e2665c5c528298dedef120882c95f63d8fc5add2
ghasem <ghasem.fattahpour@gmail.com>
2026-06-18 21:56:38 -0400

    feat: register IndexMonitorService bean

--- src/main/java/com/acme/airetrieval/config/ObserveConfig.java
+++ src/main/java/com/acme/airetrieval/config/ObserveConfig.java
@@ -1,5 +1,10 @@
 package com.acme.airetrieval.config;
 
+import com.acme.airetrieval.index.IndexMonitorService;
+import com.acme.airetrieval.index.NrtLuceneSearcher;
+import com.acme.airetrieval.ingest.BranchSyncScheduler;
+import com.acme.airetrieval.ingest.CheckpointStore;
+import com.acme.airetrieval.ingest.IndexActivityTracker;
 import com.acme.airetrieval.observe.MetricsService;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.context.annotation.Bean;
@@ -10,5 +15,14 @@ public class ObserveConfig {
     @Bean
     public MetricsService metricsService(MeterRegistry registry) {
         return new MetricsService(registry);
+    }
+
+    @Bean
+    public IndexMonitorService indexMonitorService(NrtLuceneSearcher searcher,
+                                                   CheckpointStore checkpointStore,
+                                                   BranchSyncScheduler syncScheduler,
+                                                   IndexActivityTracker activityTracker,
+                                                   ApplicationProps props) {
+        return new IndexMonitorService(searcher, checkpointStore, syncScheduler, activityTracker, props);
     }
 }
